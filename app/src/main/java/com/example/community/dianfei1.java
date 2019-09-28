ff37d5beddf7e5c4d6afef7038485c691c95fd73
13857548028 <5299398+zhang_xiao_feng@user.noreply.gitee.com>
2019-09-28 10:30:56 +0800

    电费1的记录

--- app/src/main/java/com/example/community/dianfei1.java
+++ app/src/main/java/com/example/community/dianfei1.java
@@ -14,17 +14,29 @@ public class dianfei1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dianfei1);
         Button button1=findViewById(R.id.addbtn);
-        TextView show=findViewById(R.id.show);
+        final Button chongzhi1=findViewById(R.id.chongzhi1);
+        final TextView show1=findViewById(R.id.show1);
+        final TextView delete1=findViewById(R.id.delete1);
+
+
         Intent intent=this.getIntent();
         final String gongyu = intent.getStringExtra("公寓");
         final String louhao = intent.getStringExtra("楼号");
         final String louceng = intent.getStringExtra("楼层");
         final String qinshihao = intent.getStringExtra("寝室号");
         String zong=gongyu+louhao+louceng+qinshihao;
-        if((gongyu==null)||(louhao==null)||(louceng==null)||(qinshihao==null)){
+        if ((gongyu == null) || (louhao == null) || (louceng == null) || (qinshihao == null)) {
             zong="";
+            chongzhi1.setVisibility(View.GONE);
+            delete1.setVisibility(View.GONE);;
         }
-        show.setText(zong);
+        show1.setText(zong);
+
+
+
+
+
+
         button1.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -32,7 +44,16 @@ public class dianfei1 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        delete1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                show1.setText("");
+                chongzhi1.setVisibility(View.GONE);
+                delete1.setVisibility(View.GONE);
+            }
+        });
 
 
     }
 }
+
