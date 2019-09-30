98e4450346f294076a344cb9430c2c14522d067f
13857548028 <5299398+zhang_xiao_feng@user.noreply.gitee.com>
2019-09-30 13:35:59 +0800

    电费数据库

---
+++ app/src/main/java/com/example/community/DianfeiInfo.java
@@ -0,0 +1,10 @@
+package com.example.community;
+
+public class DianfeiInfo {
+    public String Num;
+    public String gongyu;
+    public String louhao;
+    public String louceng;
+    public String qisnhihao;
+
+}
